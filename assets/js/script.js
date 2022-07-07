01eed2ce0475d1543efe041f6e2f3a4be33df7da
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-07-06 21:32:28 -0400

    set up empty functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,26 @@ var cities = [];
 var searchFormEl = document.getElementById('search-form');
 var cityInputEl = document.getElementById('city');
 var currentResultEl = document.getElementById('current-search-container');
+
+var prevSearchEl = document.getElementById('previous-search-elements');
+var currentSearchEl = document.getElementById('current-search');
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+};
+
+var saveSearchHistory = function () {
+
+};
+
+var getWeather = function(city) {
+
+};
+
+var displayWeather = function () {
+
+};
+
+var getUvIndex = function () {
+
+};
