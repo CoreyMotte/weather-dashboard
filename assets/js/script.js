a0057c141877f3721c6ac9e70606412e6459b26f
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-07-06 21:47:00 -0400

    search now gets API results

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,15 @@ var currentSearchEl = document.getElementById('current-search');
 
 var apiKey = "335c0c4e60cb6c9ae9deab5a5c9ce481";
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    var city = cityInputEl.value.trim();
-    if (city) {
-        getWeather(city);
-    }
-};
+// var formSubmitHandler = function (event) {
+//     event.preventDefault();
+//     var city = cityInputEl.value.trim();
+//     if (city) {
+//         getWeather(city);
+//     } else {
+//         alert("Please enter a city!");
+//     }
+// };
 
 var saveSearchHistory = function () {
 
@@ -24,19 +26,29 @@ var saveSearchHistory = function () {
 var getWeather = function (city) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-    fetch(apiURL)
+    fetch(apiUrl)
         .then(function (response) {
             response.json().then(function(data){
                 displayWeather(city, data);
-            })
+            });
         });
 
 };
 
-var displayWeather = function () {
-
+var displayWeather = function (search, weather) {
+    console.log(search, weather);
 };
 
 var getUvIndex = function () {
 
 };
+
+searchFormEl.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if (city) {
+        getWeather(city);
+    } else {
+        alert("Please enter a city!");
+    }
+});
