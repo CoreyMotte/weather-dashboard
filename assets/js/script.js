cb9fef45abbe7230f4077063596a318ef812c8a6
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-07-06 21:49:52 -0400

    fixed function names

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,6 @@ var currentSearchEl = document.getElementById('current-search');
 
 var apiKey = "335c0c4e60cb6c9ae9deab5a5c9ce481";
 
-// var formSubmitHandler = function (event) {
-//     event.preventDefault();
-//     var city = cityInputEl.value.trim();
-//     if (city) {
-//         getWeather(city);
-//     } else {
-//         alert("Please enter a city!");
-//     }
-// };
-
 var saveSearchHistory = function () {
 
 };
